593faf9ff8674ff8d80cd0f02a974b162f8b93e3
zeyame <zeyame14@gmail.com>
2024-08-31 18:09:51 +0300

    Added an early version of a data input page

--- client/src/routes.js
+++ client/src/routes.js
@@ -51,6 +51,8 @@ const Toasts = React.lazy(() => import('./views/notifications/toasts/Toasts'))
 
 const Widgets = React.lazy(() => import('./views/widgets/Widgets'))
 
+import DataInputPage from './views/pages/DataInputPage'
+
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
@@ -97,6 +99,8 @@ const routes = [
   { path: '/notifications/modals', name: 'Modals', element: Modals },
   { path: '/notifications/toasts', name: 'Toasts', element: Toasts },
   { path: '/widgets', name: 'Widgets', element: Widgets },
+  { path: '/data-input', name: 'Data Input', element: DataInputPage },
+
 ]
 
 export default routes

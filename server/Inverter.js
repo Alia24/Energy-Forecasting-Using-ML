6a06cc066fb3631d1cd5e9a77e6d4cd1e7730615
SuperNova74-k <kingkhaled.w@gmail.com>
2024-08-31 20:10:25 +0300

    Added Inverter.js

---
+++ server/Inverter.js
@@ -0,0 +1,8 @@
+class Inverter{
+    constructor(price, efficiency, warranty, maintenance_yearly) {
+        this.price = price
+        this.efficiency = efficiency
+        this.warranty = warranty
+        this.maintenance_yearly = maintenance_yearly
+    }
+}

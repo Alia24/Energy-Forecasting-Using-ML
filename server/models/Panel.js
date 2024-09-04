2bae3a9f0f39442f334493b030a79a2b3a88a0f6
Islam <islamalshaer.b@gmail.com>
2024-09-04 10:25:50 +0300

    refactored panel for better attribute naming

--- server/models/Panel.js
+++ server/models/Panel.js
@@ -1,10 +1,12 @@
-export class Panel {
+class Panel {
     constructor(max_power_KW, efficiency_percentage, price, warranty_years, degradation_rate_percent_over_warranty_years, maintenance_yearly) {
         this.max_power_KW = max_power_KW;
         this.efficiency_percentage = efficiency_percentage;
         this.price = price;
         this.warranty_years = warranty_years;
-        this.degradation_rate_percent_over_warranty_years = degradation_rate_percent_over_warranty_years;
-        this.maintenance_yearly = maintenance_yearly;
+        this.power_percentage_after_warrenty_years = degradation_rate_percent_over_warranty_years;
+        this.maintenance_yearly_dollars = maintenance_yearly;
     }
 }
+
+module.exports = { Panel };

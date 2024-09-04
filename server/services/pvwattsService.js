776c1070f0c7415e88ec30bff3db7a5217685f3d
Mennatallah9 <mennaa619@gmail.com>
2024-09-04 14:14:36 +0300

    Update pvwattsService to include system capacity in API request

--- server/services/pvwattsService.js
+++ server/services/pvwattsService.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 
-const getSolarData = async (latitude, longitude) => {
+const getSolarData = async (latitude, longitude, systemCapacity) => {
   const apiKey = process.env.PVWATTS_API_KEY;
-  const url = `https://developer.nrel.gov/api/pvwatts/v6.json?api_key=${apiKey}&lat=${latitude}&lon=${longitude}&system_capacity=4&azimuth=180&tilt=40&array_type=1&module_type=1&losses=10`;
+  const url = `https://developer.nrel.gov/api/pvwatts/v6.json?api_key=${apiKey}&lat=${latitude}&lon=${longitude}&system_capacity=${systemCapacity}&azimuth=180&tilt=${latitude}&array_type=0&module_type=1&losses=15`;
 
   const response = await axios.get(url);
   return response.data;

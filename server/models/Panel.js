7b53e73020ecb19a22d1a672bd5b26c78a66084c
Islam <islamalshaer.b@gmail.com>
2024-09-04 15:22:47 +0300

    refactor

--- server/models/Panel.js
+++ server/models/Panel.js
@@ -1,5 +1,5 @@
 class Panel {
-    constructor(image, name, max_power_KW, efficiency_percentage, price, lifetime, degradation_rate_percent_over_warranty_years, maintenance_yearly, installation_cost) {
+    constructor(image, name, max_power_KW, price, lifetime, degradation_rate_percent_over_warranty_years, maintenance_yearly, installation_cost) {
         this.image = image;
         this.name = name;
         this.max_power_KW = max_power_KW;

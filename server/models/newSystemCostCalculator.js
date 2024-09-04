69352749a91899007544363cda777d69cd7ae5e5
Islam <islamalshaer.b@gmail.com>
2024-09-04 11:29:25 +0300

    progress to build on

--- server/models/newSystemCostCalculator.js
+++ server/models/newSystemCostCalculator.js
@@ -1,1 +1,27 @@
 // new system cost calculator model
+const { Panel } = require('./Panel');
+const { Inverter } = require('./Inverter');
+const { Battery } = require('./Battery');
+const { UserInputs } = require('./UserInputs');
+
+class NewSystemCostCalculator{
+    constructor(panel, inverter, battery, user_inputs) {
+        this.battery = battery
+        this.panel = panel
+        this.inverter = inverter
+        this.user_inputs = user_inputs
+    }
+
+    #get_panels_details() {
+
+    }
+
+    get_details_of_new_system(){
+        let price = 0;
+        price += this.#get_panels_details()
+    }
+
+
+}
+
+module.exports = { NewSystemCostCalculator };

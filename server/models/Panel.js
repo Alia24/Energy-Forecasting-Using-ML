5eb9b4aa98b5227d70918cb72a88d60414529fc7
Islam <islamalshaer.b@gmail.com>
2024-09-04 13:07:32 +0300

    refactored attributes (this is a bad commit name sorry)

--- server/models/Panel.js
+++ server/models/Panel.js
@@ -3,13 +3,11 @@ class Panel {
         this.image = image;
         this.name = name;
         this.max_power_KW = max_power_KW;
-        this.efficiency_percentage = efficiency_percentage;
         this.price = price;
         this.lifetime = lifetime;
         this.power_percentage_after_warrenty_years = degradation_rate_percent_over_warranty_years;
         this.maintenance_yearly_dollars = maintenance_yearly;
         this.installation_cost_dollar = installation_cost;
-
     }
 }
 
